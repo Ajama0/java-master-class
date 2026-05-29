30e23c7eaf217befdcc34fd2a2e8eb484b55aa28
ajama0 <abasjama04@hotmail.com>
2026-05-29 16:30:40 +0100

    feat: implement car booking cli system

--- src/main/java/com/abas/Users/UserDAO.java
+++ src/main/java/com/abas/Users/UserDAO.java
@@ -7,8 +7,9 @@ public class UserDAO {
 
 
     static{
-        users[0] = new User(UUID.randomUUID(), "Abas");
-        users[1] = new User(UUID.randomUUID(), "james");
+        users[0] = new User(UUID.fromString("8ca51d2b-aaaf-4bf2-834a-e02964e10fc3"), "Abas");
+
+        users[1] = new User(UUID.fromString("b10d126a-3608-4980-9f9c-aa179f5cebc3"), "Jamila");
 
     }
 
